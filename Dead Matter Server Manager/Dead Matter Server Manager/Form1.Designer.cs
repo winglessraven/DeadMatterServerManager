9e1792718c27362305a9d3a4ddeb3e8bc06a1700
winglessraven <winglessravens@gmail.com>
2020-08-31 10:28:05 +0100

    Version update to 1.6.0

--- Dead Matter Server Manager/Dead Matter Server Manager/Form1.Designer.cs
+++ Dead Matter Server Manager/Dead Matter Server Manager/Form1.Designer.cs
@@ -284,7 +284,7 @@
             this.softwareVersion.Name = "softwareVersion";
             this.softwareVersion.Size = new System.Drawing.Size(69, 25);
             this.softwareVersion.TabIndex = 12;
-            this.softwareVersion.Text = "Version 1.5.1";
+            this.softwareVersion.Text = "Version 1.6.0";
             this.softwareVersion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // updateServer
